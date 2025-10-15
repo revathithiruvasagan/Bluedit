83a385d042d319792d5045516c260ffa49df5052
Revathi Thiruvasagan <131807019+revathithiruvasagan@users.noreply.github.com>
2025-10-15 07:12:27 +0530

    [Backend] Community logic Updated

---
+++ .history/Serverchat/app_20251015070059.py
@@ -0,0 +1,12 @@
+    from flask import Flask
+    from flask_cors import CORS
+    from api.auth import auth_bp
+
+    app = Flask(__name__)
+    CORS(app)
+
+    # Register routes
+    app.register_blueprint(auth_bp)
+
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=5000, debug=True)
--- .history/Serverchat/app_20251015070215.py
+++ .history/Serverchat/app_20251015070215.py
@@ -8,7 +8,6 @@ CORS(app)
 
 # Register routes
 app.register_blueprint(auth_bp)
-app.register_blueprint(community_bp)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
